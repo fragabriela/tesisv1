52c0f8b3ac540482e197c150246c000797ea30ec
francichavez <franciscachavez1996@gmail.com>
2025-06-16 17:58:42 -0300

    Agregue nuevas tablas

---
+++ database/migrations/2025_06_16_200843_alumnos.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+          Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('email')->unique();
+            $table->string('telefono');
+            $table->string('cedula');
+            $table->string('matricula');
+            $table->date('fecha_nacimiento');
+            $table->bigInteger('id_carrera')->unsigned();
+
+            $table->foreign('id_carrera')
+                ->references('id')
+                ->on('carreras');
+                
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
